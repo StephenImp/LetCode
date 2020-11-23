d17806c1d84eef6b18a3c74e0a5ca21c467a3030
wupengwen <wupengwen@yunlizhihui.com>
2020-11-23 18:32:08 +0800

    first commit

---
+++ src/main/java/com/cn/letcode/LetCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cn.letcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetCodeApplication.class, args);
+    }
+
+}
